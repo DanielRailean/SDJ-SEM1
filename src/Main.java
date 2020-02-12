bb6c8504669ac526693622ee3bfbc3c01b054d18
Daniel Railean <daniel.railean1@gmail.com>
2020-02-12 09:27:09 +0100

    Updated MAIN.JAVA

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.print("Buddy!");
+        System.out.print("WORKS?");
         System.out.print("Hello World!");
     }
 }
