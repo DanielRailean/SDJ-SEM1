a62be903aac5175ddd8e90bc0d23a269ab3e1498
Daniel Railean <daniel.railean1@gmail.com>
2020-02-12 09:06:48 +0100

    Added buddy!

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.print("Buddy!");
         System.out.print("Hello World!");
     }
 }
