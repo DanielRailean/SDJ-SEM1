394bde50128a006697d47cee68718fb21924ab08
DimitrianC <dimitrian.cebotaru@google.com>
2020-02-13 11:43:57 +0100

    seva

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         System.out.print("Buddy!");
         System.out.print("WORKS?");
         System.out.print("Hello World!");
+        System.out.print("HSSSDS!");
 
     }
 }
